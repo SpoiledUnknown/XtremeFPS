172dd00549932084f8309dc9980778f4e53da476
Spoiled Unknown <shanjusamsong254@gmail.com>
2024-04-26 16:22:35 +0530

    Completed the touch system.

---
+++ Assets/Spoiled Unknown/XtremeFPS/Scripts/Editor/FPS Input Manager Editor.cs
@@ -0,0 +1,52 @@
+/*Copyright � Non-Dynamic Studios*/
+/*2023*/
+/*Note: This is an important editor script*/
+
+using UnityEditor;
+using XtremeFPS.InputHandler;
+using UnityEngine;
+
+[CustomEditor(typeof(FPSInputManager)), CanEditMultipleObjects]
+public class FPSInputManagerEditor : UnityEditor.Editor
+{
+    FPSInputManager inputM_UI;
+    SerializedObject serInputM_UI;
+
+    private void OnEnable()
+    {
+        inputM_UI = (FPSInputManager)target;
+        serInputM_UI = new SerializedObject(inputM_UI);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serInputM_UI.Update();
+        #region Intro
+        EditorGUILayout.Space();
+        GUI.color = Color.black;
+        GUILayout.Label("Xtreme FPS Controller", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        GUI.color = Color.green;
+        GUILayout.Label("Input Manager Script", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
+        EditorGUILayout.Space();
+        GUI.color = Color.black;
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        GUI.color = Color.white;
+        #endregion
+#if UNITY_ANDROID || UNITY_IOS
+        inputM_UI.maxTouchLimit = EditorGUILayout.IntField(new GUIContent("Max Touch Limit", "Maximum number of touches that should be handled by the input manager."), inputM_UI.maxTouchLimit);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("touchDetectionMode"));
+        serializedObject.ApplyModifiedProperties();
+#endif
+        #region Update Changes
+        //Sets any changes from the prefab
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(inputM_UI);
+            Undo.RecordObject(inputM_UI, "FPC Change");
+            serInputM_UI.ApplyModifiedProperties();
+        }
+        #endregion
+    }
+
+}
